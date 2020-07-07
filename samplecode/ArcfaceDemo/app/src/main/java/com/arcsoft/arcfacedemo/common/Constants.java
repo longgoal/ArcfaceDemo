8922d5f1be5d44ca289871931b5463d70d9b66d9
liuxiyu <liuxiyu@hotmail.com>
2020-07-07 10:25:58 +0800

    1,加载图片时，出现内存不够   --android:largeHeap="true" 2,使用申请的APP ID和SDK KEY  --测试可以激活

--- samplecode/ArcfaceDemo/app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
+++ samplecode/ArcfaceDemo/app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -1,9 +1,19 @@
 package com.arcsoft.arcfacedemo.common;
 
 public class Constants {
-
-    public static final String APP_ID = "官网获取的APP_ID";
-    public static final String SDK_KEY = "官网获取的SDK_KEY";
+    /*
+    https://ai.arcsoft.com.cn/
+    135...
+    1QAZ2wsx
+    --驾校培训
+    APP ID:HoyG6sn4RD9kL58JrTkMmuqHLMUEShsbrySY62xkAeey
+    --人证核验Android
+    SDK KEY:8BkteH1WcFmMzkUSJSdsX81UMrtU9QkaSxpgQKWbbdRS
+    --人脸识别（ArcFace）Android
+    SDK KEY:8BkteH1WcFmMzkUSJSdsX81E34N8uBPG1Mt7kDrn6mR5
+    */
+    public static final String APP_ID = "HoyG6sn4RD9kL58JrTkMmuqHLMUEShsbrySY62xkAeey";
+    public static final String SDK_KEY = "8BkteH1WcFmMzkUSJSdsX81E34N8uBPG1Mt7kDrn6mR5";
 
     /**
      * IR预览数据相对于RGB预览数据的横向偏移量，注意：是预览数据，一般的摄像头的预览数据都是 width > height
